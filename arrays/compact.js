26bac55a0fb1f36afa0dc2969d6832f22a6a3d82
@shmulim <shmuel.markel@gmail.com>
2015-06-24 00:54:12 -0700

    Update compact.js

--- arrays/compact.js
+++ arrays/compact.js
@@ -1,3 +1,6 @@
+// Returns a copy of the array with all falsy values removed.
+// In JavaScript, false, null, 0, "", undefined and NaN are all falsy.
+
 var array = [0, 1, false, 2, '', 3, null, undefined, NaN];
 
 Array.prototype.compact = function(){
