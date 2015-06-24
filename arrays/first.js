35fa142a7104ad90c7095759f45b37c478e0e85c
@shmulim <shmuel.markel@gmail.com>
2015-06-23 20:46:43 -0700

    Update first.js

--- arrays/first.js
+++ arrays/first.js
@@ -1,6 +1,13 @@
-function first(arr){
-  return arr[0];
+// Returns the first element of an array.
+// Passing n will return the first n elements of the array.
+
+function first(arr, n){
+  n = n || 1;
+  return arr.slice(0, n);
 }
 
 first([5, 4, 3, 2, 1]);
-// 5
+// [5]
+
+first([5, 4, 3, 2, 1], 3);
+// [5, 4, 3]
