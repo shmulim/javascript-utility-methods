77ddabc84c2dda6fd8451e1ff516c60645f636a0
@shmulim <shmuel.markel@gmail.com>
2015-06-26 01:22:11 -0700

    Update rest.js

--- arrays/rest.js
+++ arrays/rest.js
@@ -3,13 +3,13 @@
 
 var array = [5, 4, 3, 2, 1];
 
-Array.prototype.rest = function(i) {
+Array.prototype._rest = function(i) {
     i = i || 1;
     return this.slice(i);
 };
 
-array.rest();
+array._rest();
 // [4, 3, 2, 1]
 
-array.rest(3);
+array._rest(3);
 // [2, 1]
