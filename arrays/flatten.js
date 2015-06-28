fa27fb1b758723db511704521abbaf2229010ad2
@shmulim <shmuel.markel@gmail.com>
2015-06-27 23:25:10 -0700

    Create flatten.js

---
+++ arrays/flatten.js
@@ -0,0 +1,22 @@
+// Flattens a nested array (the nesting can be to any depth).
+// If you pass shallow, the array will only be flattened a single level.
+
+var arr = [[1], 2, [[3, 4], 5], [[[]]],[[[6]]], 7, 8, []];
+
+Array.prototype._flatten = function(shallow) {
+  if (shallow) {
+    return this.reduce(function(a, b) {
+      return a.concat(b);
+    }, []);
+  } else {
+    return this.reduce(function(acc, val) {
+      return acc.concat(val.constructor === Array ? val.flatten() : val);
+    }, []);
+  }
+};
+
+arr._flatten();
+// [1, 2, 3, 4, 5, 6, 7, 8]
+
+arr._flatten(true);
+// [1, 2, [3, 4], 5, [[]], [[6]], 7, 8]
